f69934ddb9fb1d3ddfd6425f3139f3de9c4e6ae6
Dani Quesada <daniela.dudyfit@gmail.com>
2023-03-10 21:54:31 +0100

    done

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,28 @@
 <script setup lang="ts">
 import { ref } from "vue";
-import PersonCard from "./components/PersonCard.vue";
-import { IPeople } from "./models/People.interface";
+import ColorPicker from "./components/ColorPicker.vue";
 
-const peopleList: IPeople[] = [
-  { name: "Ana", lastName: "García", age: 32, email: "anita@gmail.com" },
-  { name: "Pedro", lastName: "López", age: 22, email: "pedri@gmail.com" },
-  { name: "Diego", lastName: "Martínez", age: 27, email: "diegs@gmail.com" },
-  { name: "Claudia", lastName: "Ruiz", age: 29, email: "clau9@gmail.com" },
-];
+const bgColor = ref("");
 
-const clickedUser = ref<number | undefined>();
+const changeBg = (color: string) => (bgColor.value = color);
 </script>
 
 <template>
-  <div>
-    <p @click="clickedUser = 0" class="name">{{ peopleList[0].name }}</p>
-    <p @click="clickedUser = 1" class="name">{{ peopleList[1].name }}</p>
-    <p @click="clickedUser = 2" class="name">{{ peopleList[2].name }}</p>
-    <p @click="clickedUser = 3" class="name">{{ peopleList[3].name }}</p>
-    <div v-if="clickedUser !== undefined">
-      <PersonCard :user="peopleList[clickedUser]" />
-    </div>
+  <div :class="[bgColor]" class="bg">
+    <ColorPicker @bg-change="changeBg" />
   </div>
 </template>
+
+<style>
+.bg {
+  height: 200px;
+  width: 200px;
+  padding: 10px;
+}
+.red {
+  background-color: red;
+}
+.blue {
+  background-color: blue;
+}
+</style>
---
+++ src/components/ColorPicker.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+const emit = defineEmits<{
+  (e: "bgChange", color: string): void;
+}>();
+
+const onColorClick = (color: string) => emit("bgChange", color);
+</script>
+
+<template>
+  <div>
+    <button @click="onColorClick('red')">RED</button>
+    <button @click="onColorClick('blue')">BLUE</button>
+  </div>
+</template>
